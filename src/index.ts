5efdb0798d99db6d4cf2ac76efd6277f400edb78
Erik Onarheim <erik.onarheim@gmail.com>
2020-04-17 16:10:55 -0500

    For giggles :dagger:

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
-import { Engine } from "excalibur";
+import { Engine, vec, ParticleEmitter, Color, Loader, Graphics, EmitterType } from "excalibur";
+import sword from '../img/sword.png';
 
 Engine._useWebGL = true;
 const game = new Engine();
+const loader = new Loader();
 
-game.start();
+const image = new Graphics.RawImage(sword);
+const sprite = Graphics.Sprite.from(image);
+sprite.origin = vec(.5, .5);
+
+const emitter = new ParticleEmitter({
+    pos: vec(game.halfDrawWidth, game.halfDrawHeight),
+    emitterType: EmitterType.Circle,
+    radius: 1,
+    minVel: 417,
+    maxVel: 589,
+    minAngle: Math.PI,
+    maxAngle: Math.PI * 2,
+    isEmitting: false,
+    emitRate: 10,
+    opacity: 1,
+    fadeFlag: true,
+    particleLife: 2465,
+    maxSize: 1.5,
+    minSize: 0.5,
+    acceleration: vec(0, 460),
+    sprite: sprite,
+    particleRotationalVelocity: Math.PI / 2,
+    randomRotation: true
+  });
+
+emitter.isEmitting = true;
+game.add(emitter);
+
+loader.addResource(image);
+
+game.start(loader);
 
 (window as any).game = game;
