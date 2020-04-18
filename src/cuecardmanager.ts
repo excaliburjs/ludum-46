22c97749121b0ed766fe23daae9717dafe67f9d2
Unknown <Jasonlh.Harris@gmail.com>
2020-04-18 16:41:25 -0500

    CardManager calculates card positions

--- src/cuecardmanager.ts
+++ src/cuecardmanager.ts
@@ -2,44 +2,52 @@ import { CueCard } from "./cuecard";
 import { Scene, vec } from "excalibur";
 
 export class CueCardManager {
-  private numberOfCueCards: number = 3;
-  private cueCards: Array<CueCard> = new Array<CueCard>();
+  private firstCueCard: CueCard;
+  private secondCueCard: CueCard;
+  private thirdCueCard: CueCard;
+  private cardPadding: number = 20;
+  private cardTopOffset: number = 10;
   private cueCardHeight: number = 100;
   private cueCardWidth: number = 300;
+  private scene: Scene;
 
   constructor(scene: Scene) {
-    this.cueCards.push(
-      new CueCard({
-        lifeTime: 10,
-        requiredCostume: {},
-        requiredLocation: {},
-        requiredProp: {},
-        cueCardHeight: this.cueCardHeight,
-        cueCardWidth: this.cueCardWidth,
-        cueCardLoc: vec(10, 10),
-      }),
-      new CueCard({
-        lifeTime: 10,
-        requiredCostume: {},
-        requiredLocation: {},
-        requiredProp: {},
-        cueCardHeight: this.cueCardHeight,
-        cueCardWidth: this.cueCardWidth,
-        cueCardLoc: vec(this.cueCardWidth * 2, 10),
-      }),
-      new CueCard({
-        lifeTime: 10,
-        requiredCostume: {},
-        requiredLocation: {},
-        requiredProp: {},
-        cueCardHeight: this.cueCardHeight,
-        cueCardWidth: this.cueCardWidth,
-        cueCardLoc: vec(this.cueCardWidth * 3, 10),
-      })
-    );
+    this.scene = scene;
+    this.firstCueCard = new CueCard({
+      lifeTime: 10,
+      requiredCostume: {},
+      requiredLocation: {},
+      requiredProp: {},
+      cueCardHeight: this.cueCardHeight,
+      cueCardWidth: this.cueCardWidth,
+      cueCardLoc: this._calculateCueCardPosition(1),
+    });
+    this.secondCueCard = new CueCard({
+      lifeTime: 10,
+      requiredCostume: {},
+      requiredLocation: {},
+      requiredProp: {},
+      cueCardHeight: this.cueCardHeight,
+      cueCardWidth: this.cueCardWidth,
+      cueCardLoc: this._calculateCueCardPosition(2),
+    });
+    this.thirdCueCard = new CueCard({
+      lifeTime: 10,
+      requiredCostume: {},
+      requiredLocation: {},
+      requiredProp: {},
+      cueCardHeight: this.cueCardHeight,
+      cueCardWidth: this.cueCardWidth,
+      cueCardLoc: this._calculateCueCardPosition(3),
+    });
+    this.scene.add(this.firstCueCard);
+    this.scene.add(this.secondCueCard);
+    this.scene.add(this.thirdCueCard);
+  }
 
-    for (let card of this.cueCards) {
-      scene.add(card);
-    }
+  private _calculateCueCardPosition(cardNumber: number) {
+    const padding = this.cardPadding * cardNumber;
+    const cardWidth = this.cueCardWidth * (cardNumber - 1);
+    return vec(padding + cardWidth, this.cardTopOffset);
   }
 }
