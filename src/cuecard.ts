4b5c5404236e7108a73e26202ef023fad96c1d80
casmith <carolyn.smith@renaissance.com>
2020-04-18 12:47:55 -0500

    addd timer rect

--- src/cuecard.ts
+++ src/cuecard.ts
@@ -1,39 +1,45 @@
 import { Actor, Color, Engine, Graphics } from "excalibur";
 
 export class CueCard extends Actor {
-    constructor() {
-        super();
-        const rect = new Graphics.Rect({
-            width:100,
-            height:100,
-            color: Color.White
-        });
+  constructor() {
+    super();
+    const rect = new Graphics.Rect({
+      width: 100,
+      height: 100,
+      color: Color.White,
+    });
 
-        this.graphics.add(rect);
+    const timerRect = new Graphics.Rect({
+      width: 100,
+      height: 100,
+      color: Color.White,
+    });
 
-        //Background (grey)
-        const background = this.graphics.createLayer({name: 'background', order:-1});
-        background.show(rect);
-        //Timer (white)
-        this.graphics.createLayer({name: 'timer', order:1});
+    this.graphics.add(rect);
 
-        //Symbol Loc1
-        //Symbol Loc2
-        //Symbol Loc3
-        this.graphics.createLayer({name:'goals', order:2});
+    //Background (grey)
+    const background = this.graphics.createLayer({
+      name: "background",
+      order: -1,
+    });
+    background.show(rect);
+    //Timer (white)
+    const timer = this.graphics.createLayer({ name: "timer", order: 1 });
+    timer.show(timerRect);
+    //Symbol Loc1
+    //Symbol Loc2
+    //Symbol Loc3
+    this.graphics.createLayer({ name: "goals", order: 2 });
 
+    this.graphics.getLayer("background")?.graphic;
+  }
 
-        this.graphics.getLayer('background')?.graphic
-    }
+  public onInitialize(engine: Engine) {
+    let prop = {};
+    let costume = {};
+    let location = {};
+    let timer = {};
+  }
 
-    public onInitialize(engine: Engine) {
-        let prop = {};
-        let costume = {};
-        let location = {};
-        let timer = {};
-    }
-
-    public update(engine: Engine, delta: number) {
-
-    }
+  public update(engine: Engine, delta: number) {}
 }
