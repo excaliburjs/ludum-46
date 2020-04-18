07c0a812b724bc6bca2e8d2ac28af13523186724
alanag13 <alan.grgic@gmail.com>
2020-04-18 14:03:28 -0500

    rebase

--- src/player.ts
+++ src/player.ts
@@ -5,12 +5,16 @@ import {
   Input,
   PostDrawEvent,
   PostUpdateEvent,
+  Graphics,
+  Random,
+  Resource,
 } from "excalibur";
-import config from "./config";
+import Config from "./config";
+import { Resources } from "./resources";
 
 export class Player extends Actor {
   constructor(x: number, y: number) {
-    super(x, y, config.PlayerWidth, config.PlayerHeight);
+    super(x, y, Config.PlayerWidth, Config.PlayerHeight);
   }
 
   public onInitialize(engine: Engine) {
@@ -22,22 +26,22 @@ export class Player extends Actor {
       switch (keyHeld.key) {
         case Input.Keys.Up:
         case Input.Keys.W:
-          this.vel.setTo(this.vel.x, -config.PlayerVel);
+          this.vel.setTo(this.vel.x, -Config.PlayerVel);
           this.graphics.show("walkUp");
           break;
         case Input.Keys.Down:
         case Input.Keys.S:
-          this.vel.setTo(this.vel.x, config.PlayerVel);
+          this.vel.setTo(this.vel.x, Config.PlayerVel);
           this.graphics.show("walkDown");
           break;
         case Input.Keys.Left:
         case Input.Keys.A:
-          this.vel.setTo(-config.PlayerVel, this.vel.y);
+          this.vel.setTo(-Config.PlayerVel, this.vel.y);
           this.graphics.show("walkLeft");
           break;
         case Input.Keys.Right:
         case Input.Keys.D:
-          this.vel.setTo(config.PlayerVel, this.vel.y);
+          this.vel.setTo(Config.PlayerVel, this.vel.y);
           this.graphics.show("walkRight");
           break;
       }
@@ -52,7 +56,7 @@ export class Player extends Actor {
           break;
         case Input.Keys.Down:
         case Input.Keys.S:
-          this.vel.setTo(this.vel.x, config.PlayerVel);
+          this.vel.setTo(this.vel.x, Config.PlayerVel);
           this.graphics.show("down");
           break;
         case Input.Keys.Left:
@@ -74,5 +78,70 @@ export class Player extends Actor {
     //  this.on('postdraw', (evt: PostDrawEvent) => {
     //     this._selectSprite.draw(evt.ctx, -this._selectSprite.naturalWidth / 2, this._selectSprite.naturalHeight / 2);
     //  });
+  }
+  private _setupDrawing(engine: Engine) {
+    var sheet = Graphics.SpriteSheet.fromGrid({
+      image: this.getCharSprite(),
+      grid: {
+        rows: 1,
+        columns: 10,
+        spriteHeight: 45,
+        spriteWidth: 45,
+      },
+    });
+    var sprites = sheet.sprites;
+    var downSprite = sprites[0];
+    var upSprite = sprites[3];
+    var leftSprite = sprites[7];
+    var rightSprite = sprites[9];
+
+    downSprite.origin?.setTo(0, 0.2);
+    upSprite.origin?.setTo(0, 0.2);
+    leftSprite.origin?.setTo(0, 0.2);
+    rightSprite.origin?.setTo(0, 0.2);
+
+    this.graphics.add("down", downSprite);
+    this.graphics.add("up", upSprite);
+    this.graphics.add("left", leftSprite);
+    this.graphics.add("right", rightSprite);
+
+    var walkDownAnim = Graphics.Animation.fromSpriteSheet(
+      sheet,
+      [0, 1, 0, 2],
+      180
+    );
+    walkDownAnim.origin?.setTo(0, 0.2);
+    this.graphics.add("walkDown", walkDownAnim);
+
+    var walkUpAnim = Graphics.Animation.fromSpriteSheet(
+      sheet,
+      [3, 4, 3, 5],
+      180
+    );
+    walkUpAnim.origin?.setTo(0, 0.2);
+    this.graphics.add("walkUp", walkUpAnim);
+
+    var walkLeftAnim = Graphics.Animation.fromSpriteSheet(sheet, [7, 6], 200);
+    walkLeftAnim.origin?.setTo(0, 0.2);
+    this.graphics.add("walkLeft", walkLeftAnim);
+
+    var walkRightAnim = Graphics.Animation.fromSpriteSheet(sheet, [9, 8], 200);
+    walkRightAnim.origin?.setTo(0, 0.2);
+    this.graphics.add("walkRight", walkRightAnim);
+
+    this.setDrawing("down");
+  }
+
+  public getCharSprite(): Graphics.RawImage {
+    var gameRandom = new Random(Date.now());
+    let charSheets = [];
+    for (var r in Resource) {
+      if (r.search("charSheet") != -1) {
+        charSheets.push(Resources[r]);
+      }
+    }
+    var randCharSheets = gameRandom.shuffle(charSheets);
+    var result = randCharSheets[0];
+    return <Graphics.RawImage>result;
   }
 }
