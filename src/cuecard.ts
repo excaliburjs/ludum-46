a1f880a6cf33abddc64d7f5b6c37028fadad78dd
casmith <carolyn.smith@renaissance.com>
2020-04-18 13:46:11 -0500

    Add stage left and right icons

--- src/cuecard.ts
+++ src/cuecard.ts
@@ -1,57 +1,51 @@
-import { Actor, Color, Engine, Graphics } from "excalibur";
-
+import { Actor, Color, Engine, Graphics, Resource } from "excalibur";
+import { Resources } from "./resources";
+import { RawImage } from "~../lib/excalibur/build/dist/Graphics";
 export class CueCard extends Actor {
-  private background: Graphics.GraphicsLayer;
-  private timerRect: Graphics.Rect;
-  private timer: number;
-  private lifeTime: number;
-  private cueCardWidth: number = 1000;
+  private cues: Graphics.GraphicsLayer;
 
   constructor() {
     super();
-    this.timer = this.lifeTime = 10;
+    const rect = new Graphics.Rect({
+      width: 100,
+      height: 100,
+      color: Color.White,
+    });
 
-    //#region  background
-    this.background = this.graphics.createLayer({
+    const timerRect = new Graphics.Rect({
+      width: 100,
+      height: 100,
+      color: Color.White,
+    });
+
+    this.graphics.add(rect);
+
+    //Background (grey)
+    const background = this.graphics.createLayer({
       name: "background",
       order: -1,
     });
-    const rect = new Graphics.Rect({
-      width: this.cueCardWidth,
-      height: 100,
-      color: Color.Gray,
-    });
-    this.background.show
-    //#endregion background
-
-    //#region timer
-    const timerLayer = this.graphics.createLayer({ name: "timer", order: 1 });
-    this.timerRect = new Graphics.Rect({
-      width: this.cueCardWidth,
-      height: 100,
-      color: Color.White,
-    });(rect);
-    timerLayer.show(this.timerRect);
-    //#endregion timer
-
-    //#region Symbol Layer
+    background.show(rect);
+    //Timer (white)
+    const timer = this.graphics.createLayer({ name: "timer", order: 1 });
+    timer.show(timerRect);
     //Symbol Loc1
     //Symbol Loc2
     //Symbol Loc3
-    this.graphics.createLayer({ name: "goals", order: 2 });
-    //#endregion
 
+    this.cues = this.graphics.createLayer({ name: "cues", order: 2 });
+
+    let stageLeft = new Graphics.RawImage("./assets/stage-left.png");
+    let stageLeftSprite = Graphics.Sprite.from(stageLeft);
+    this.cues.show(stageLeftSprite);
   }
 
   public onInitialize(engine: Engine) {
+    let prop = {};
+    let costume = {};
+    let location = {};
+    let timer = {};
   }
 
-  public update(engine: Engine, delta: number) {
-    this.timer -= delta / 1000;
-    this.timerRect.width = 1000 * this.timer / this.lifeTime;
-
-    if(this.timer <= 0) {
-        this.kill();
-    }
-  }
+  public update(engine: Engine, delta: number) {}
 }
--- src/resources.ts
+++ src/resources.ts
@@ -1,8 +1,11 @@
-import sword from '../img/sword.png';
+import sword from "../img/sword.png";
+import stageLeft from "./assets/stage-left.png";
+import stageRight from "./assets/stage-right.png";
 
 import { Graphics, Resource } from "excalibur";
 
-
-export const Resources: {[key: string]: Resource<any>} = {
-    swordImage: new Graphics.RawImage(sword)
- };
+export const Resources: { [key: string]: Resource<any> } = {
+  swordImage: new Graphics.RawImage(sword),
+  stageLeftImage: new Graphics.RawImage(stageLeft),
+  stageRightImage: new Graphics.RawImage(stageRight),
+};
