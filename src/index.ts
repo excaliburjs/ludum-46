685c0a8c5842b3074baea0726b42404312b48820
jedeen <jedeen@users.noreply.github.com>
2020-04-18 13:51:51 -0500

    Set game height and width

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { Theater } from "./theater-scene";
 import { Resources } from "./resources";
 import { CueCard } from "./cuecard";
 import { newgame } from "./session";
+import Config from "./config";
 
 Engine._useWebGL = true;
-const game = new Engine();
+const game = new Engine({ height: Config.GameHeight, width: Config.GameWidth });
 const loader = new Loader();
 for (let r in Resources) {
   loader.addResource(Resources[r]);
