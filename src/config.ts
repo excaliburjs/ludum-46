df3a747ec06d5db2f7a5a60816c82db2e7e36d4b
jedeen <jedeen@users.noreply.github.com>
2020-04-18 12:15:26 -0500

    Add game width and height to config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 export default {
   AnalyticsEndpoint: "https://ludum46stats.azurewebsites.net/api/LD46Stats",
+
   PlayerWidth: 25,
   PlayerHeight: 25,
   PlayerVel: 100,
+
+  GameWidth: 1280,
+  GameHeight: 720,
 };
