2641f73bd8c5a4ecd8227635464cb6c6de92fc98
Kamran Ayub <kamran.ayub@gmail.com>
2020-04-18 20:41:33 -0500

    Add solid walls

--- src/theater-scene.ts
+++ src/theater-scene.ts
@@ -1,6 +1,8 @@
 import { Scene, Engine, TileMap } from "excalibur";
 import { Resources } from "./resources";
-import { TiledResource } from "excalibur-tiled";
+import { ITiledMapLayer } from "excalibur-tiled";
+
+const LAYER_IMPASSABLE = "walls";
 
 export class Theater extends Scene {
   public map!: TileMap;
@@ -10,8 +12,27 @@ export class Theater extends Scene {
   }
 
   public onInitialize(engine: Engine) {
-    this.map = (Resources.map as TiledResource).getTileMap(40, 168);
+    this.map = Resources.map.getTileMap(40, 168);
     this.map.data.forEach((c) => (c.transform.z = -3));
     this.add(this.map);
+
+    Resources.map.data.layers.forEach((layer) => {
+      this.collectSolidTiles(layer);
+    });
+  }
+
+  collectSolidTiles(layer: ITiledMapLayer) {
+    if (
+      layer.name !== LAYER_IMPASSABLE ||
+      typeof layer.data == "string" ||
+      !layer.data
+    )
+      return;
+
+    for (let i = 0; i < layer.data.length; i++) {
+      if (layer.data[i] !== 0) {
+        this.map.data[i].solid = true;
+      }
+    }
   }
 }
