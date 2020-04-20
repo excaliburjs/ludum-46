7a5cd24f6e413c1811f4550904e62318ac3202b3
Jason Harris <Jasonlh.Harris@gmail.com>
2020-04-20 15:14:25 -0500

    EndScreen text

--- src/gameover.ts
+++ src/gameover.ts
@@ -13,7 +13,7 @@ import { DialogCard } from "./dialogCard";
 import Config from "./config";
 import config from "./config";
 import { Button } from "./button";
-import {newgame, stats} from "./session";
+import { newgame, stats } from "./session";
 import { SoundManager } from "./soundManager";
 
 export class GameOver extends Actor {
@@ -29,6 +29,12 @@ export class GameOver extends Actor {
 
   onInitialize(engine: Engine) {
     this.engine = engine;
+    this.backshadowLayer = this.graphics.createLayer({ name: "backshadow", order: 1 });
+    this.backShadow = new Graphics.Rect({
+      width: Config.GameWidth + 500,
+      height: Config.GameHeight + 500,
+      color: Color.fromRGB(51, 51, 51, 0.5)
+    });
   }
 
   public updateEndScreen() {
@@ -54,15 +60,10 @@ export class GameOver extends Actor {
     this.scene.add(this.card);
     this.card.z = 100;
 
-    this.backshadowLayer = this.graphics.createLayer({name: "backshadow", order: 1});
-    this.backShadow = new Graphics.Rect({
-      width: Config.GameWidth + 500,
-      height: Config.GameHeight + 500,
-      color: Color.fromRGB(51, 51, 51, 0.5)
-    });
+
     this.z = 100;
 
-    this.button = new Button("Play Again?",{
+    this.button = new Button("Play Again?", {
       pos: cardPos,
       width: 200,
       height: 100
@@ -74,7 +75,7 @@ export class GameOver extends Actor {
         actor.kill();
       }
       this.engine.removeScene(this.scene);
-      this.button.pos.add(vec(10000,10000));
+      this.button.pos.add(vec(10000, 10000));
       newgame(this.engine);
     });
     this.scene.add(this.button);
@@ -84,6 +85,7 @@ export class GameOver extends Actor {
 
   show() {
     this.backshadowLayer.show(this.backShadow);
+    this.pos = vec(0, 0);
     this.card.actions.easeTo(
       Config.GameWidth / 2,
       Config.GameHeight / 2,
